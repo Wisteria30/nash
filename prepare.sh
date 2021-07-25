65973ad36f100ca620787ade0acc0fc6f1886664
Wisteria30 <skymars0327@yahoo.co.jp>
2021-07-25 22:03:35 +0900

    dataのダウンロードをprepareに追加

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,7 @@ wget http://qwone.com/~jason/20Newsgroups/20news-18828.tar.gz
 tar -xvf 20news-18828.tar.gz
 rm 20news-18828.tar.gz
 cd ..
+./download_fp.sh
 pip install -r requirements.txt
 python -c "import nltk; nltk.download('punkt')"
 wandb login
